a8eef109d88fe6b858cf21580db17e848bc3989b
Erik Tomlinson <eptomlinson@gmail.com>
2020-11-01 19:33:49 -0600

    added heroku credentials, if else statement

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const mysql = require('mysql');
 
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+    connection =mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'rootroot',
+        database: 'burger_db'
+    });
+};
+
 const db = require('./models');
 
 const routes = require('./models');
